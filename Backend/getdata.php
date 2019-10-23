c30245f5b61e3b85ed1ef90fa6753c90c1fcc666
hit27 <rohitsarate1027@gmail.com>
2019-10-23 11:22:04 +0530

    collect data fropm data base and html page

---
+++ Backend/getdata.php
@@ -0,0 +1,16 @@
+<?php
+//connect to  database
+require '../Database/conection.php';
+require '../Database/database.php';
+
+// get database connection
+$conection = new conection();
+$database = $conection->getConnection();
+$formdata = new mydatabase($database);
+//get post data from Html file
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+//assign value to $form_data 
+$formdata->UserName=$request->username;
+$formdata->password=$request->password;
+?>
